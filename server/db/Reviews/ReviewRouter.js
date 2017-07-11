8b107930c1dc744af7f6a9404233cea95c1e36cd
J6K <jmkang06@gmail.com>
2017-07-11 15:08:28 -0700

    [Feat] -review router working

--- server/db/Reviews/ReviewRouter.js
+++ server/db/Reviews/ReviewRouter.js
@@ -1,23 +1,29 @@
 import express from 'express';
-// import ReviewQueries from './ReviewQueries.js';
+import ReviewQueries from './ReviewQueries';
 
 const router = express.Router();
-// const Review = new ReviewQueries();
+const Review = new ReviewQueries();
+let result;
 
-router.get('/getReviews/:user_id', async (req,res) {
+router.get('/getReviews/:user_id', async (req, res) => {
   try {
-    
+    result = await Review.fetchReviews(req.params.user_id);
+    res.status(200).send(result);
   } catch (error) {
-    console.log('Error in getReviewsforUser route')
+    console.log('Error in getReviewsforUser route');
     res.status(500).send(error);
   }
 });
 
-router.post('/postReview', async (req, res) {
+router.post('/postReview', async (req, res) => {
   try {
-    
+    console.log('this is the req.body ', req.body)
+    result = await Review.postReviews(req.body);
+    res.status(200).send(result);
   } catch (error) {
     console.log('Error in postReview route');
     res.status(500).send(error);
   }
 });
+
+module.exports = router;
--- server/router/index.js
+++ server/router/index.js
@@ -4,5 +4,6 @@ router.use('/users', require('../db/Users/UserRouter'));
 router.use('/cities', require('../db/City/CityRouter'));
 router.use('/accomplishments', require('../db/Accomplishments/AccomplishmentRouter'));
 // router.use('/workouts', require('../db/Workout/WorkoutRouter'));
+router.use('/reviews', require('../db/Reviews/ReviewRouter'));
 
 module.exports = router;
