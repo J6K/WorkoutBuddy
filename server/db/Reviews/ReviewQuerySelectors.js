90767383058a991a65a2f7a58a9fe437a4d5d11c
J6K <jmkang06@gmail.com>
2017-07-11 15:08:28 -0700

    [Feat] -review router working

--- server/db/Reviews/ReviewQuerySelectors.js
+++ server/db/Reviews/ReviewQuerySelectors.js
@@ -4,7 +4,9 @@ export default class ReviewQuerySelector {
     return `SELECT * FROM REVIEWS WHERE sender_id = ${id} OR receiver_id = ${id}`;
   }
 
-  postReviews(review, rating, sender_id, receiver_id) {
+  postReviews({ review, rating, sender_id, receiver_id }) {
+    sender_id = parseInt(sender_id);
+    receiver_id = parseInt(receiver_id);
     return `INSERT INTO REVIEWS (review, rating, sender_id, receiver_id) VALUES ('${review}', ${rating}, ${sender_id}, ${receiver_id})`;
   }
 
