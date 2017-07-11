4a4869245ab003059fab745921283cc7ebd9184b
J6K <jmkang06@gmail.com>
2017-07-10 18:55:04 -0700

    [Feat] - starting review queries

--- server/db/Reviews/ReviewQuerySelectors.js
+++ server/db/Reviews/ReviewQuerySelectors.js
@@ -4,7 +4,8 @@ export default class ReviewQuerySelector {
     return `SELECT * FROM REVIEWS WHERE sender_id = ${id} OR receiver_id = ${id}`;
   }
 
-  postReviews(id, sender_id, receiver_id) {
-    return `INSERT INTO REVIEWS ()`
+  postReviews(review, rating, sender_id, receiver_id) {
+    return `INSERT INTO REVIEWS (review, rating, sender_id, receiver_id) VALUES ('${review}', ${rating}, ${sender_id}, ${receiver_id})`;
   }
+
 }
