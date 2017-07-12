06cc5b93dac2191be9495c57e2dc0649276a7e25
J6K <jmkang06@gmail.com>
2017-07-12 00:01:21 -0700

    [Fix] - minor changes to review query

--- server/db/Reviews/ReviewQuerySelectors.js
+++ server/db/Reviews/ReviewQuerySelectors.js
@@ -5,8 +5,6 @@ export default class ReviewQuerySelector {
   }
 
   postReviews({ review, rating, sender_id, receiver_id }) {
-    sender_id = parseInt(sender_id);
-    receiver_id = parseInt(receiver_id);
     return `INSERT INTO REVIEWS (review, rating, sender_id, receiver_id) VALUES ('${review}', ${rating}, ${sender_id}, ${receiver_id})`;
   }
 
